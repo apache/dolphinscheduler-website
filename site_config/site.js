e8ba6d827c31517f30182ce21b8843f4788c4651
khadgarmage <khadgar.mage@outlook.com>
2019-09-28 16:39:12 +0800

    fix site prefix and picture is too large (#6)
    * Update README.md
    
    * Create meetup_2019_10_26.md

--- site_config/site.js
+++ site_config/site.js
@@ -1,6 +1,6 @@
 // 全局的一些配置
 export default {
-  rootPath: '/incubator-dolphinscheduler-website', // 发布到服务器的根目录，需以/开头但不能有尾/，如果只有/，请填写空字符串
+  rootPath: '', // 发布到服务器的根目录，需以/开头但不能有尾/，如果只有/，请填写空字符串
   port: 8080, // 本地开发服务器的启动端口
   domain: 'dolphinscheduler.incubator.apache.org', // 站点部署域名，无需协议和path等
   defaultSearch: 'google', // 默认搜索引擎，baidu或者google
