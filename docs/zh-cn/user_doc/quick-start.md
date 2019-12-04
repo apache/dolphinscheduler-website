a919c8201e51d64755d07dc267f84ceba1b4b7bb
qiaozhanwei <qiaozhanwei@outlook.com>
2019-12-04 20:06:18 +0800

    Revert "Modify quick start, user manual  "

--- docs/zh-cn/user_doc/quick-start.md
+++ docs/zh-cn/user_doc/quick-start.md
@@ -4,10 +4,11 @@
   >地址：192.168.xx.xx:8888 用户名密码：admin/dolphinscheduler123
 
 <p align="center">
-   <img src="/img/login.png" width="60%" />
+   <img src="/img/login.jpg" width="60%" />
  </p>
 
 * 创建队列
+
 <p align="center">
    <img src="/img/create-queue.png" width="60%" />
  </p>
@@ -23,18 +24,9 @@
  </p>
 
   * 创建告警组
+
  <p align="center">
     <img src="/img/mail_edit.png" width="60%" />
-  </p>
-
- * 创建Worker分组
- <p align="center">
-    <img src="/img/worker_group.png" width="60%" />
-  </p>
- 
- * 创建token令牌
- <p align="center">
-    <img src="/img/creat_token.png" width="60%" />
   </p>
 
   * 使用普通用户登录
--- site_config/docs.js
+++ site_config/docs.js
@@ -65,100 +65,6 @@ export default {
                 ]
             }
         ],
-<<<<<<< HEAD
-      },
-      {
-        title: 'Release',
-        children: [
-          {
-            title: 'Upgrade',
-            link: '/en-us/docs/release/upgrade.html',
-          }
-        ],
-      },
-      {
-        title: 'FAQ',
-        children: [
-          {
-            title: 'FAQ',
-            link: '/en-us/docs/faq.html',
-          }
-        ]
-      }
-    ],
-    barText: 'Documentation',
-  },
-  'zh-cn': {
-    sidemenu: [
-      {
-        title: '用户文档',
-        children: [
-          {
-            title: '快速上手',
-            link: '/zh-cn/docs/user_doc/quick-start.html',
-          },
-          {
-            title: '后端部署',
-            link: '/zh-cn/docs/user_doc/backend-deployment.html',
-          },
-          {
-            title: '前端部署',
-            link: '/zh-cn/docs/user_doc/frontend-deployment.html',
-          },
-          {
-            title: '系统手册',
-            link: '/zh-cn/docs/user_doc/system-manual.html',
-          },
-        ],
-      },
-      {
-        title: '开发者指南',
-        children: [
-          {
-            title: '架构设计',
-            link: '/zh-cn/docs/developer_guide/architecture-design.html',
-          },
-          {
-            title: '后端开发',
-            link: '/zh-cn/docs/developer_guide/backend-development.html',
-          },
-          {
-            title: '插件开发',
-            link: '/zh-cn/docs/developer_guide/architecture-design.html',
-          },
-          {
-            title: '前端开发',
-            link: '/zh-cn/docs/developer_guide/frontend-development.html',
-          },
-          {
-            title: '接口文档',
-            target: '_blank',
-            link: 'http://106.75.43.194:8888/easyscheduler/doc.html?language=zh_CN&lang=zh'
-          },
-        ],
-      },
-      {
-        title: '版本发布',
-        children: [
-          {
-            title: '升级',
-            link: '/zh-cn/docs/release/upgrade.html',
-          }
-        ],
-      },
-      {
-        title: 'FAQ',
-        children: [
-          {
-            title: 'FAQ',
-            link: '/zh-cn/docs/faq.html',
-          }
-        ]
-      }
-    ],
-    barText: '文档'
-  }
-=======
         barText: 'Documentation',
     },
     'zh-cn': {
@@ -229,5 +135,4 @@ export default {
         ],
         barText: '文档'
     }
->>>>>>> website/master
 };
