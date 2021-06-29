6f1e12a01058e6518e75aaae828201a50f5c2c2d
Kirs <acm_master@163.com>
2021-06-30 00:43:15 +0800

    add dev docs (#390)
    * add dev docs
    
    * Update alert_spi.md
    
    * Update alert_spi.md
    
    * add dev docs
    
    * add dev docs
    
    Co-authored-by: dailidong <dailidong66@gmail.com>

---
+++ site_config/docsdev.js
@@ -0,0 +1,39 @@
+export default {
+  'en-us': {
+    sidemenu: [
+      {
+        title: 'Deployment Document',
+        children: [
+          {
+            title: 'Alert-SPI',
+            link: '/en-us/docs/dev/user_doc/alert_spi.html',
+          },
+          {
+            title: 'Registry-SPI',
+            link: '/en-us/docs/dev/user_doc/registry_spi.html',
+          },
+        ],
+      },
+
+    ],
+    barText: 'Documentation',
+  },
+  'zh-cn': {
+    sidemenu: [
+      {
+        title: '开发文档',
+        children: [
+          {
+            title: 'Alert-SPI',
+            link: '/zh-cn/docs/dev/user_doc/alert_spi.html',
+          },
+          {
+            title: 'Registry-SPI',
+            link: '/zh-cn/docs/dev/user_doc/registry_spi.html',
+          },
+        ],
+      },
+    ],
+    barText: '文档',
+  },
+};
