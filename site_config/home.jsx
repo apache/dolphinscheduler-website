66fb5d1668713e60bae2f7fe82dc8e25229b6747
Kirs <acm_master@163.com>
2021-08-02 23:39:34 +0800

    new Committer wenjun (#414)
    * new Committer wen jun
    
    * Update new_committer_wenjun.md
    
    Co-authored-by: David <dailidong66@gmail.com>

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -55,6 +55,13 @@ export default {
       title: '事件 & 新闻',
       list: [
         {
+          img: '/img/apache.jpg',
+          title: 'Wenjun Ruan：如何成为Apache DolphinScheduler Committer',
+          content: '7.29,来自 eBay 的 Wenjun 同学正式成为Apache DolphinSchedulerCommitter,此文将为大家分享他的开源旅程，以及他是如何成为DolphinScheduler的Committer',
+          dateStr: '2021-07-29',
+          link: '/zh-cn/blog/new_committer_wenjun.html',
+        },
+        {
           img: '/img/dailidong.png',
           title: 'Apache DolphinScheduler(Incubating) 1.3.0新特性及Roadmap路线直播',
           content: 'Apache DolphinScheduler(Incubating) 1.3.0新特性及Roadmap路线在线直播，700多人在线',
