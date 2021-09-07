096744f002e7f5cd6caa42dfa8a74b5e135521f1
David <dailidong66@gmail.com>
2021-09-07 11:52:33 +0800

    add 1.3.8-release doc (#431)
    * update error metadata version
    
    * add docker deployment doc in 1.3.4
    
    * update description about docker
    
    * update word "deploy" -> "deployment"
    
    * fix error，use unzip
    
    * update small error in the docker deployment
    
    * update description about DolphinScheduler
    
    * [ANNOUNCE] Welcome to our new committer: BoYiZhang
    
    * add blog: FAQ of DolphinScheduler
    
    * [ANNOUNCE] 1.3.8 doc added
    
    * [ANNOUNCE] 1.3.8 doc added
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/architecture-design.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/architecture-design.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/architecture-design.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/architecture-design.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/architecture-design.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    * Update docs/en-us/1.3.8/user_doc/ambari-integration.md
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>

--- site_config/site.js
+++ site_config/site.js
@@ -1,16 +1,16 @@
 // 全局的一些配置
 export default {
-  rootPath: '', // 发布到服务器的根目录，需以/开头但不能有尾/，如果只有/，请填写空字符串
-  port: 8080, // 本地开发服务器的启动端口
-  domain: 'dolphinscheduler.apache.org', // 站点部署域名，无需协议和path等
-  copyToDist: ['asset', 'img', 'file', '.asf.yaml', 'sitemap.xml', '.nojekyll', '.htaccess'], // 当build发布时，需要额外复制到dist目录的资源，默认有：index.html, 404.html, en-us, zh-cn, build
-  docsLatest: '1.3.6', // docs的最新版本，当docs有多个版本时支持latest访问，空则不启用此特性
-  defaultSearch: 'google', // 默认搜索引擎，baidu或者google
+  rootPath: '',
+  port: 8080,
+  domain: 'dolphinscheduler.apache.org',
+  copyToDist: ['asset', 'img', 'file', '.asf.yaml', 'sitemap.xml', '.nojekyll', '.htaccess'],
+  docsLatest: '1.3.8',
+  defaultSearch: 'google', // default search engine
   defaultLanguage: 'en-us',
   'en-us': {
     pageMenu: [
       {
-        key: 'home', // 用作顶部菜单的选中
+        key: 'home',
         text: 'HOME',
         link: '/en-us/index.html',
       },
@@ -21,8 +21,13 @@ export default {
         children: [
           {
             key: 'docs0',
-            text: 'latest(1.3.6)',
+            text: 'latest(1.3.8)',
             link: '/en-us/docs/latest/user_doc/quick-start.html',
+          },
+          {
+            key: 'docs138',
+            text: '1.3.8',
+            link: '/en-us/docs/1.3.8/user_doc/quick-start.html',
           },
           {
             key: 'docs136',
@@ -228,8 +233,13 @@ export default {
         children: [
           {
             key: 'docs0',
-            text: 'latest(1.3.6)',
+            text: 'latest(1.3.8)',
             link: '/zh-cn/docs/latest/user_doc/quick-start.html',
+          },
+          {
+            key: 'docs138',
+            text: '1.3.8',
+            link: '/zh-cn/docs/1.3.8/user_doc/quick-start.html',
           },
           {
             key: 'docs136',
