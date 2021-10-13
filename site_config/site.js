da0d55f6a5aea908e75e7b2339f4a60410d3c7bf
Jiajie Zhong <zhongjiajie955@hotmail.com>
2021-10-13 17:24:02 +0800

    Change dev default page to quick start

--- site_config/site.js
+++ site_config/site.js
@@ -77,7 +77,7 @@ export default {
           {
             key: 'docsdev',
             text: 'dev',
-            link: '/en-us/docs/dev/user_doc/dev_quick_start.html',
+            link: '/en-us/docs/dev/user_doc/quick-start.html',
           },
         ],
       },
@@ -289,7 +289,7 @@ export default {
           {
             key: 'docsdev',
             text: 'dev',
-            link: '/zh-cn/docs/dev/user_doc/dev_quick_start.html',
+            link: '/zh-cn/docs/dev/user_doc/quick-start.html',
           },
         ],
       },
