eea055e975c4892af935676ea99e98638dad908d
Jiajie Zhong <zhongjiajie955@hotmail.com>
2021-10-13 17:13:59 +0800

    Copy doc from 1.3.8 to dev

--- site_config/docsdev.js
+++ site_config/docsdev.js
@@ -5,6 +5,121 @@ export default {
         title: 'Deployment Document',
         children: [
           {
+            title: 'Hardware Environment',
+            link: '/en-us/docs/dev/user_doc/hardware-environment.html',
+          },
+          {
+            title: 'Standalone Deployment',
+            link: '/en-us/docs/dev/user_doc/standalone-deployment.html',
+          },
+          {
+            title: 'Cluster Deployment',
+            link: '/en-us/docs/dev/user_doc/cluster-deployment.html',
+          },
+          {
+            title: 'Docker Deployment',
+            link: '/en-us/docs/dev/user_doc/docker-deployment.html',
+          },
+          {
+            title: 'Kubernetes Deployment',
+            link: '/en-us/docs/dev/user_doc/kubernetes-deployment.html',
+          },
+          {
+            title: 'SkyWalking-Agent Deployment',
+            link: '/en-us/docs/dev/user_doc/skywalking-agent-deployment.html',
+          },
+        ],
+      },
+      {
+        title: 'User Manual',
+        children: [
+          {
+            title: 'Quick Start',
+            link: '/en-us/docs/dev/user_doc/quick-start.html',
+          },
+          {
+            title: 'User Manual',
+            link: '/en-us/docs/dev/user_doc/system-manual.html',
+          },
+          {
+            title: 'Open API',
+            link: '/en-us/docs/dev/user_doc/open-api.html',
+          },
+          {
+            title: 'Flink',
+            link: '/en-us/docs/dev/user_doc/flink-call.html',
+          },
+          {
+            title: 'Parameter Introduction',
+            link: '/en-us/docs/dev/user_doc/parameters-introduction.html',
+          },
+        ],
+      },
+      {
+        title: 'Architecture Design',
+        children: [
+          {
+            title: 'Metadata',
+            link: '/en-us/docs/dev/user_doc/metadata-1.3.html',
+          },
+          {
+            title: 'Architecture Design',
+            link: '/en-us/docs/dev/user_doc/architecture-design.html',
+          },
+          {
+            title: 'Configuration File',
+            link: '/en-us/docs/dev/user_doc/configuration-file.html',
+          },
+          {
+            title: 'Task Structure',
+            link: '/en-us/docs/dev/user_doc/task-structure.html',
+          },
+          {
+            title: 'Load Balance',
+            link: '/en-us/docs/dev/user_doc/load-balance.html',
+          },
+        ],
+      },
+      {
+        title: 'Integration',
+        children: [
+          {
+            title: 'Ambari Integration',
+            link: '/en-us/docs/dev/user_doc/ambari-integration.html',
+          },
+        ],
+      },
+      {
+        title: 'Upgrade',
+        children: [
+          {
+            title: 'Upgrade',
+            link: '/en-us/docs/dev/user_doc/upgrade.html',
+          },
+        ],
+      },
+      {
+        title: 'Expansion/Reduction',
+        children: [
+          {
+            title: 'Capacity expansion and reduction',
+            link: '/en-us/docs/dev/user_doc/expansion-reduction.html',
+          },
+        ],
+      },
+      {
+        title: 'FAQ',
+        children: [
+          {
+            title: 'FAQ',
+            link: '/en-us/docs/release/faq.html',
+          },
+        ],
+      },
+      {
+        title: 'To be Classification',
+        children: [
+          {
             title: 'Dev-Quick-Start',
             link: '/en-us/docs/dev/user_doc/dev_quick_start.html',
           },
@@ -26,14 +141,119 @@ export default {
           },
         ],
       },
-
     ],
     barText: 'Documentation',
   },
   'zh-cn': {
     sidemenu: [
       {
-        title: '开发文档',
+        title: '部署文档',
+        children: [
+          {
+            title: '软硬件环境建议配置',
+            link: '/zh-cn/docs/dev/user_doc/hardware-environment.html',
+          },
+          {
+            title: '单机部署(Standalone)',
+            link: '/zh-cn/docs/dev/user_doc/standalone-deployment.html',
+          },
+          {
+            title: '集群部署(Cluster)',
+            link: '/zh-cn/docs/dev/user_doc/cluster-deployment.html',
+          },
+          {
+            title: 'Docker部署(Docker)',
+            link: '/zh-cn/docs/dev/user_doc/docker-deployment.html',
+          },
+          {
+            title: 'Kubernetes部署(Kubernetes)',
+            link: '/zh-cn/docs/dev/user_doc/kubernetes-deployment.html',
+          },
+          {
+            title: 'SkyWalking-Agent部署(SkyWalking)',
+            link: '/zh-cn/docs/dev/user_doc/skywalking-agent-deployment.html',
+          },
+        ],
+      },
+      {
+        title: '用户手册',
+        children: [
+          {
+            title: '快速上手',
+            link: '/zh-cn/docs/dev/user_doc/quick-start.html',
+          },
+          {
+            title: '用户手册',
+            link: '/zh-cn/docs/dev/user_doc/system-manual.html',
+          },
+          {
+            title: 'API调用',
+            link: '/zh-cn/docs/dev/user_doc/open-api.html',
+          },
+          {
+            title: 'Flink调用',
+            link: '/zh-cn/docs/dev/user_doc/flink-call.html',
+          },
+          {
+            title: '参数介绍',
+            link: '/zh-cn/docs/dev/user_doc/parameters-introduction.html',
+          },
+        ],
+      },
+      {
+        title: '架构设计',
+        children: [
+          {
+            title: '元数据文档',
+            link: '/zh-cn/docs/dev/user_doc/metadata-1.3.html',
+          },
+          {
+            title: '架构设计',
+            link: '/zh-cn/docs/dev/user_doc/architecture-design.html',
+          },
+          {
+            title: '配置文件',
+            link: '/zh-cn/docs/dev/user_doc/configuration-file.html',
+          },
+          {
+            title: '任务结构',
+            link: '/zh-cn/docs/dev/user_doc/task-structure.html',
+          },
+          {
+            title: '负载均衡',
+            link: '/zh-cn/docs/dev/user_doc/load-balance.html',
+          },
+        ],
+      },
+      {
+        title: '版本升级',
+        children: [
+          {
+            title: '升级',
+            link: '/zh-cn/docs/dev/user_doc/upgrade.html',
+          },
+        ],
+      },
+      {
+        title: '扩/缩容',
+        children: [
+          {
+            title: '扩/缩容',
+            link: '/zh-cn/docs/dev/user_doc/expansion-reduction.html',
+          },
+        ],
+      },
+      {
+        title: 'FAQ',
+        children: [
+          {
+            title: 'FAQ',
+            link: '/zh-cn/docs/release/faq.html',
+          },
+        ],
+      },
+      {
+        title: '待分类文档',
         children: [
           {
             title: 'Dev-Quick-Start',
@@ -62,7 +282,8 @@ export default {
           {
             title: 'Dev-Run',
             link: '/zh-cn/docs/dev/user_doc/dev_run.html',
-          },        ],
+          },
+        ],
       },
     ],
     barText: '文档',
