143e021b2fcd7dc75a577740e6191969bfba2ba7
nielifeng <53861852+nielifeng@users.noreply.github.com>
2021-10-25 17:53:45 +0800

    add blog.js
    add blog.js

--- site_config/blog.js
+++ site_config/blog.js
@@ -3,11 +3,18 @@ export default {
     barText: 'Blog',
     postsTitle: 'All posts',
     list: [
+          {
+        title: 'DolphinScheduler Vulnerability Explanation',
+        author: 'Debar Chen',
+        dateStr: '2021-10-26',
+        desc: 'DolphinScheduler Vulnerability Explanation',
+        link: '/en-us/blog/DolphinScheduler Vulnerability Explanation.html',
+      },
       {
         title: 'Introducing Apache DolphinScheduler 1.3.9, StandaloneServer is Available!',
         author: 'Debar Chen',
         dateStr: '2021-10-22',
-        desc: 'Apache Dolphin Scheduler(Incubating) will organize a meetup in Shanghai  2019.10.26. Welcome to register.',
+        desc: 'Introducing Apache DolphinScheduler 1.3.9, StandaloneServer is Available!',
         link: '/en-us/blog/Introducing Apache DolphinScheduler 1.3.9.html',
       },
       {
@@ -47,6 +54,13 @@ export default {
     barText: '博客',
     postsTitle: '所有文章',
     list: [
+     {
+        title: 'DolphinScheduler漏洞情况说明',
+        author: '徐玮毅',
+        dateStr: '2021-10-26',
+        desc: 'DolphinScheduler漏洞情况说明',
+        link: '/zh-cn/blog/DolphinScheduler漏洞情况说明.html',
+      },
       {
         title: 'Apache DolphinScheduler 架构演进及开源经验分享',
         author: '徐玮毅',
