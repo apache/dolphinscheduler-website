e142d4371f9a885bde49073181fb14496d0c3ad3
lifeng <53861852+nielifeng@users.noreply.github.com>
2022-01-07 16:40:08 +0800

    updata (#624)
    * updata
    
    * Update Awarded_most_popular_project_in_2021.md

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -55,8 +55,7 @@ export default {
       title: '事件 & 新闻',
       list: [
         {
-          img: 'https://imgpp.com/images/2022/01/07/1.md.png',
-
+          img: 'https://imgpp.com/image/9K6nJ',
           title: 'Apache DolphinScheduler 获评 2021 年度「最受欢迎项目」',
           content: '近日，由 OSCHINA 举办的「2021 OSC 中国开源项目」评选活动公布了评选结果。...',
           dateStr: '2022-1-7',
