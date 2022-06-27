30fc92e97f0a2feadcad302fdcdbedc289906a7f
zhuxt2015 <594754793@qq.com>
2022-06-27 21:33:33 +0800

    [comunity] Add new users yili (#803)

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -269,6 +269,9 @@ export default {
         {
           img: '/img/ourusers/sutpc.png',
         },
+        {
+          img: '/img/ourusers/yili.png',
+        },
       ],
     },
     userreview: {
