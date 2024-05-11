30f338f3970b62e179d4c93ace6d680ed3941ab6
Gallardot <gallardot@apache.org>
2024-05-11 23:04:32 +0800

    Update index.jsx (#958)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Divider, Button, Space } from "antd";
 import {
@@ -11,6 +12,24 @@ import "./index.scss";
 
 const Footer = () => {
   const { locale, t } = useTranslation();
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+    script.async = true;
+    script.dataset.websiteId = "e3268e5d-c0f1-4e71-819c-c60ebb2215a7";
+    script.dataset.projectName = "Apache DolphinScheduler";
+    script.dataset.projectColor = "#0097E0";
+    script.dataset.projectLogo = "images/logo_400x400.jpg";
+    script.dataset.modalDisclaimer = "This is a custom LLM for Apache DolphinScheduler with access to all developer Documentation, Blog, GitHub issues and discussions.";
+    script.dataset.modalExampleQuestions = "Why we need DolphinScheduler?,How to deploy DolphinScheduler?,How to submit task?,How to contribute?";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
   return (
     <footer className="footer">
       <section className="footer-content">
