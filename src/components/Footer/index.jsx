35653a09c964cc0b6ce6e11404c1cbdec21b2b34
Gallardot <gallardot@apache.org>
2024-06-06 09:52:06 +0800

    chore: try to fix logo url error on widget (#961)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -20,16 +20,11 @@ const Footer = () => {
     script.dataset.websiteId = "e3268e5d-c0f1-4e71-819c-c60ebb2215a7";
     script.dataset.projectName = "Apache DolphinScheduler";
     script.dataset.projectColor = "#0097E0";
-    script.dataset.projectLogo = "images/logo_400x400.jpg";
+    script.dataset.projectLogo = "https://dolphinscheduler.apache.org/images/logo_400x400.jpg";
     script.dataset.modalDisclaimer = "This is a custom LLM for Apache DolphinScheduler with access to all developer Documentation, Blog, GitHub issues and discussions.";
     script.dataset.modalExampleQuestions = "Why we need DolphinScheduler?,How to deploy DolphinScheduler?,How to submit task?,How to contribute?";
 
     document.body.appendChild(script);
-
-    const meta = document.createElement('meta');
-    meta.httpEquiv = "Content-Security-Policy";
-    meta.content = "frame-src 'self' https://www.google.com/;";
-    document.head.appendChild(meta);
 
     return () => {
       document.body.removeChild(script);
